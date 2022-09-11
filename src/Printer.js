b9e9815a2df361b78529697e92cf85aacd3260bc
rburcea <robert.burcea@gmail.com>
2022-09-11 17:52:17 +0300

    reverse the order jokes are shown

--- src/Printer.js
+++ src/Printer.js
@@ -10,8 +10,9 @@ import SetBox from './SetBox'
 export default function Printer(props) {
   const screen = useScreen();
   const changeScreen = useSetScreen();
+  const reverseJokes = screen.jokes.bits.map((val, index, array) => array[array.length - 1 - index]);
   return props.choice === 'bits' ? (<div className="printer">
-      {screen.jokes.bits?.length > 0 ? screen.jokes.bits.map((joke) => {
+      {screen.jokes.bits?.length > 0 ? reverseJokes.map((joke) => {
           return <JokeBox joke={joke} key={joke.id}/>
       }) : <>No jokes to display, write one</>}
       <Footer />
